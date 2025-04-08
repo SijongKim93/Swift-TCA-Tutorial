e2c27353223bbfae2b61fc90beaf9a95be180582
SijongKim93 <rlatlwhd456@gmail.com>
2025-04-08 23:54:07 +0900

    Test 로직 추가

---
+++ TCA_Tutorial/TCA_TutorialTests.swift
@@ -0,0 +1,41 @@
+//
+//  TCA_TutorialTests.swift
+//  TCA_TutorialTests
+//
+//  Created by 김시종 on 4/8/25.
+//
+
+import ComposableArchitecture
+import Testing
+
+@MainActor
+struct CounterFeatureTests {
+    @Test
+    func basics() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) {
+            $0.count = 1
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
+    }
+    
+    @Test
+    func timer() async {
+        let clock = TestClock()
+        
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+    }
+}
